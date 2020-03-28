3a6d269bd070489af26eac242520a7de1f7275b7
Hussien <hussienashraf99@gmail.com>
2020-03-29 01:10:55 +0200

    test

--- OnlineStorePlatform/OnlineStorePlatform/Controllers/ValuesController.cs
+++ OnlineStorePlatform/OnlineStorePlatform/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace OnlineStorePlatform.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Hussien1", "value2" };
         }
 
         // GET api/values/5
