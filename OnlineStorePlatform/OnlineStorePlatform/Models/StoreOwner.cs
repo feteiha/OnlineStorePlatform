3143db3702617e50af9b542dd9485cbdaa76f72a
Khaled ElHadary <khaledelhadary@outlook.com>
2020-03-29 01:19:39 +0200

    Implemented Class Diagram

---
+++ OnlineStorePlatform/OnlineStorePlatform/Models/StoreOwner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineStorePlatform.Models
+{
+    public class StoreOwner : User, StandardUserInterface
+    {
+        public StoreOwner(DB_User u) : base(u)
+        {
+        }
+        public bool register(User user)
+        {
+            return false;
+        }
+    }
+}
