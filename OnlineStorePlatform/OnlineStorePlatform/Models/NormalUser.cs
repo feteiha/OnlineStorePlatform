0ce19f71e51a241a97826b86a74c3cd24163d59f
Hatemmamdoh <36468246+Hatemmamdoh@users.noreply.github.com>
2020-03-29 02:09:08 +0200

    Add normal user and Store owner

--- OnlineStorePlatform/OnlineStorePlatform/Models/NormalUser.cs
+++ OnlineStorePlatform/OnlineStorePlatform/Models/NormalUser.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using OnlineStorePlatform.Models;
-
+using OnlineStorePlatform.Controllers;
 
 namespace OnlineStorePlatform.Models
 {
@@ -12,9 +12,28 @@ namespace OnlineStorePlatform.Models
         public NormalUser(DB_User u) : base(u)
         {
         }
-        public bool register(User user)
+        public bool register()
         {
-            return false;
+            bool canAdd = true; 
+            List<User> allUsers;
+            DatabaseController dataBase = new DatabaseController() ;
+            allUsers = dataBase.getAllUsers();
+            for (int i = 0; i < allUsers.Count; i++)
+            {
+                if (this.username == allUsers[i].username)
+                    canAdd = false;
+            }
+            if (!canAdd || this.password.Length < 8 || this.age < 18)
+            {
+                return false;
+            }
+            else
+            {
+                dataBase.addUser(this);
+            }
+
+
+            return true;
         }
     }
 }
--- OnlineStorePlatform/OnlineStorePlatform/Models/StoreOwner.cs
+++ OnlineStorePlatform/OnlineStorePlatform/Models/StoreOwner.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using OnlineStorePlatform.Controllers;
+using OnlineStorePlatform.Models;
 
 namespace OnlineStorePlatform.Models
 {
@@ -10,9 +12,28 @@ namespace OnlineStorePlatform.Models
         public StoreOwner(DB_User u) : base(u)
         {
         }
-        public bool register(User user)
+        public bool register()
         {
-            return false;
+            bool canAdd = true;
+            List<User> allUsers;
+            DatabaseController dataBase = new DatabaseController();
+            allUsers = dataBase.getAllUsers();
+            for (int i = 0; i < allUsers.Count; i++)
+            {
+                if (this.username == allUsers[i].username)
+                    canAdd = false;
+            }
+            if (!canAdd || this.password.Length < 8 || this.age < 18)
+            {
+                return false;
+            }
+            else
+            {
+                dataBase.addUser(this);
+            }
+
+
+            return true;
         }
     }
 }
