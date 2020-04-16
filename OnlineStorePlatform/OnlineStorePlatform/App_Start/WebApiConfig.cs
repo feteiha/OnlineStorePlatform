9e307e600506f9f6c07fbd8748213c2178b7d240
Hussien <hussienashraf99@gmail.com>
2020-04-16 20:33:06 +0200

    Revert "try middleware"
    This reverts commit b24c8fdc9482f98ffa7f73c2731c2f6d289ad936.

--- OnlineStorePlatform/OnlineStorePlatform/App_Start/WebApiConfig.cs
+++ OnlineStorePlatform/OnlineStorePlatform/App_Start/WebApiConfig.cs
@@ -1,8 +1,6 @@
-﻿using Newtonsoft.Json.Serialization;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace OnlineStorePlatform
@@ -21,9 +19,6 @@ namespace OnlineStorePlatform
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
-
-			var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
-			jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-		}
+        }
     }
 }
