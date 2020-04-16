2a09f3aa0d9b42988c50ee21f313082332a07965
Khaled ElHadary <khaledelhadary@outlook.com>
2020-04-16 21:16:37 +0200

    Add token middleware

---
+++ OnlineStorePlatform/OnlineStorePlatform/Authentication_Middlware/AuthenticationMiddleware.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.Owin.Security.OAuth;
+using OnlineStorePlatform.Models;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace OnlineStorePlatform.Authentication_Middlware
+{
+    public class AuthenticationMiddleware : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var user = User.login(context.UserName, context.Password);
+            if (user == null)
+            {
+                context.SetError("invalid_grant", "Provided username and password is incorrect");
+                return;
+            }
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim(ClaimTypes.Role, user.GetType().Name));
+            identity.AddClaim(new Claim(ClaimTypes.Name, user.username));
+            identity.AddClaim(new Claim("Email", user.email));
+            context.Validated(identity);
+        }
+    }
+}
